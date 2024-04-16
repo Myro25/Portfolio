401c8f8488eb3ed14b90588cc2175399fa94cb6f
Myro25 <132125266+Myro25@users.noreply.github.com>
2024-04-16 17:19:28 +0200

    Amélioration

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-/* -----------------------------------------
-  Have focus outline only for keyboard users 
- ---------------------------------------- */
 
 const handleFirstTab = (e) => {
-  if(e.key === 'Tab') {
+  if (e.key === 'Tab') {
     document.body.classList.add('user-is-tabbing')
 
     window.removeEventListener('keydown', handleFirstTab)
